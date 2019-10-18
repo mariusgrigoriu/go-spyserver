88db5058eec529adb18bf5a40b54c046080ee8ff
Marius Grigoriu <marius.grigoriu@me.com>
2019-10-17 22:29:43 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# go-spyserver
-Simple spyserver for Go
+# SpyServer for Go
+
+Create simple, local test servers that records requests and responds with pre-configured results.
+
+## Why use this
+
+Test code making http calls without having a slow, unreliable network in the way.
+Simplify testing when making calls to external services.
+
+Go's httptest package is good, but this library gives you a few more benefits:
+
+1. You don't need to point your tests to a different URL.
+2. You can test that your client code closes the response body.
+3. You can simulate various errors that may happen with the transport.
+
+## How to use
+
+First, create a spyserver like this:
+```
+response := &http.Response{
+    StatusCode: http.StatusOK,
+	Body:       body,
+}
+spy := spyserver.New(response, nil)
+```
+
+Next, set the Transport in your http client to the spyserver and call it:
+```
+client := &http.Client{
+    Transport: spy,
+}
+client.Do(request)
+```
+
+You can inspect the request sent to the server in your tests:
+```
+spy.GetRequest()
+```
+
+You can even test that you close the response body in your production code.
+```
+body := spyserver.NewCloseDetectorFromString(jsonString)
+
+// Use the body the response you configure with your spy server
+// Then just inject the spy server as usual and call your code
+
+body.IsClosed() // returns true if you closed the response body
+```
---
+++ spyserver.go
@@ -0,0 +1,71 @@
+package spyserver
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+// SpyServer implements the http.RoundTripper interface to serve a custom
+// http.Response or error while capturing the request for later inspection
+// A pointer to this type can be assigned to http.Client.Transport to
+// override the default RoundTripper
+type SpyServer struct {
+	request  *http.Request
+	response *http.Response
+	err      error
+}
+
+// New takes a http.Response and error that will be later returned during the http.RoundTrip
+func New(response *http.Response, err error) *SpyServer {
+	return &SpyServer{response: response, err: err}
+}
+
+// RoundTrip implements the http.RoundTripper interface
+func (s *SpyServer) RoundTrip(req *http.Request) (*http.Response, error) {
+	s.request = req
+	return s.response, s.err
+}
+
+// GetRequest returns the http.Request that was passed during the last RoundTrip
+func (s *SpyServer) GetRequest() *http.Request {
+	return s.request
+}
+
+// CloseDetector is an io.ReadCloser that tracks whether Close has been called.
+// This struct is useful because the behavior of Close after the first call is undefined.
+type CloseDetector struct {
+	io.ReadCloser
+	closed bool
+}
+
+// NewCloseDetector wraps an existing io.ReadCloser.
+func NewCloseDetector(rc io.ReadCloser) *CloseDetector {
+	var closed bool
+	return &CloseDetector{rc, closed}
+}
+
+// NewCloseDetectorFromString wraps a NopCloser containing the string
+func NewCloseDetectorFromString(s string) *CloseDetector {
+	return NewCloseDetector(ioutil.NopCloser(strings.NewReader(s)))
+}
+
+// Read is a pass-through function that delegates to the wrapped Read function.
+func (cd *CloseDetector) Read(p []byte) (n int, err error) {
+	return cd.ReadCloser.Read(p)
+}
+
+// Close delegates to the wrapped Close function and tracks whether the function was called.
+func (cd *CloseDetector) Close() error {
+	cd.closed = true
+	if cd.ReadCloser == nil {
+		return nil
+	}
+	return cd.ReadCloser.Close()
+}
+
+// IsClosed returns whether Close was called
+func (cd *CloseDetector) IsClosed() bool {
+	return cd.closed
+}
